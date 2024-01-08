8fc0fbd73b249ef6a51b11461dceca2931f683e3
memmalino <memmalino@gmail.com>
2024-01-08 11:40:10 +0000

    updated views

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
-"""This script defines the route of the app_views blueprint"""
-
+"""
+module creates a route /status on any object app_views
+return: JSON: "status: OK"
+"""
 from api.v1.views import app_views
 from flask import jsonify
-import models
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+classes = {"amenities": Amenity, "cities": City, "places": Place,
+           "reviews": Review, "states": State, "users": User}
 
 
-@app_views.route('/status', strict_slashes=False)
-def show_status():
-    """This returns the status of the app"""
-    return jsonify({"status": "OK"})
+@app_views.route("/status", strict_slashes=False)
+def status():
+    """
+    Returns OK if JSON works
+    """
+    j_status = {"status": "OK"}
+    return jsonify(j_status)
 
 
-@app_views.route('/stats')
-def display_status():
-    """This returns the status of each model"""
-    model = models.storage
-    res = {'amenities': model.count("Amenity"),
-           'cities': model.count("City"),
-           'places': model.count("Place"),
-           'reviews': model.count("Review"),
-           'states': model.count("State"),
-           'users': model.count("User")}
-    return jsonify(res)
+@app_views.route("/stats", strict_slashes=False)
+def count_all():
+    """
+    Returns OK if JSON works
+    """
+    return jsonify({
+        name: storage.count(obj) for name, obj in classes.items()
+    })
