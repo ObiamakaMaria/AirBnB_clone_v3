38cab740c1da0152b98b840d52f15956d99c1a9e
ObiamakaMaria <obiamaka0101@gmail.com>
2024-01-07 18:22:32 -0800

    updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""define the router of the app_views blueprint"""
+"""This script defines the route of the app_views blueprint"""
 
 from api.v1.views import app_views
 from flask import jsonify
@@ -7,19 +7,19 @@ import models
 
 
 @app_views.route('/status', strict_slashes=False)
-def get_status():
-    """return the status of the app"""
+def show_status():
+    """This returns the status of the app"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats')
-def status_page():
-    """return the status of each model"""
+def display_status():
+    """This returns the status of each model"""
     model = models.storage
-    ret = {'amenities': model.count("Amenity"),
+    res = {'amenities': model.count("Amenity"),
            'cities': model.count("City"),
            'places': model.count("Place"),
            'reviews': model.count("Review"),
            'states': model.count("State"),
            'users': model.count("User")}
-    return jsonify(ret)
+    return jsonify(res)
