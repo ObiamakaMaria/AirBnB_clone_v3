898f60df9b3fdcff07c10f8539cfb696d369c14b
memmalino <memmalino@gmail.com>
2024-01-08 11:50:43 +0000

    updated app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
-"""The script creates of a REST API"""
-
+"""
+module to create Flask app that works with the API
+"""
 from models import storage
-from api.v1.views import app_views
 from flask import Flask, jsonify
 from flask_cors import CORS
+from api.v1.views import app_views
 import os
 
-
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_app(exc):
-    """This method closes the app """
+def tear_down(self):
+    """
+    tears down
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def error_handler(e):
-    """This returns a 404 page"""
-    data = {"error": "Not found"}
-    return jsonify(data), 404
+def not_found(error):
+    """
+    error handler for 404
+    """
+    return (jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = os.environ.get('HBNB_API_PORT', 5000)
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
